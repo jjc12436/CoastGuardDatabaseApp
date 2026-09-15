0bf0af97f3a4520c73d33aaf81f3df890461d4c5
Jameson Cooper <jmclmwi@gmail.com>
2026-09-15 14:25:14 -0500

    Update project attribution and ignore local secrets

--- CoastGuardDatabaseApp/Properties/AssemblyInfo.cs
+++ CoastGuardDatabaseApp/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CoastGuardDatabaseApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CoastGuardDatabaseApp")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2026")]
+[assembly: AssemblyCopyright("Copyright © 2026 Jameson Cooper")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
